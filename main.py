98ec3c47f4f5d362541b7986b3a1d448338619fc
Muskan013 <151116933+Muskan013@users.noreply.github.com>
2024-10-06 21:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ import streamlit as st
 import plotly.express as px
 from streamlit_option_menu import option_menu
 
-# Load saved models
-diabetes_model = pickle.load(open('C:/Users/FATHIMA MUSKAN/OneDrive/Desktop/Multiple disease prediction system/diabetes_model.save', 'rb'))
-heart_disease_model = pickle.load(open('C:/Users/FATHIMA MUSKAN/OneDrive/Desktop/Multiple disease prediction system/heart_disease_model.save', 'rb'))
-parkinsons_model = pickle.load(open('C:/Users/FATHIMA MUSKAN/OneDrive/Desktop/Multiple disease prediction system/parkinsons_model.save', 'rb'))
+# Set page configuration
+st.set_page_config(page_title="Health Assistant",
+                   layout="wide",
+                   page_icon="🧑‍⚕️")
 
+    
+# getting the working directory of the main.py
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+# loading the saved models
+
+diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
+
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
+
+parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
 # Sidebar for navigation
 with st.sidebar:
     selected = option_menu(
